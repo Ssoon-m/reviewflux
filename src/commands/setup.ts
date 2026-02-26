197133b3066d3c80f5f873368a5d0937bbf326f4
oding-ai <tkksm3@gmail.com>
2026-02-26 15:46:39 +0900

    refactor: remove legacy setup command file

--- src/commands/setup.ts
+++
@@ -1,5 +0,0 @@
-import { parseSetupOptions, runSetup } from "../cli/legacy.js";
-
-export async function runSetupCommand(args: string[]): Promise<void> {
-  await runSetup(parseSetupOptions(args));
-}
